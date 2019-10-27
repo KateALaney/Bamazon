fa2645df5695e5806682030751dbc5eb33a3d21d
Kate Laney <kate.a.laney@gmail.com>
2019-10-27 14:26:41 -0400

    working view, exit

--- bamazon.sql
+++ bamazon.sql
@@ -8,15 +8,16 @@ CREATE TABLE products (
 item_id INTEGER AUTO_INCREMENT NOT NULL,
 PRIMARY KEY (item_id),
 product_name VARCHAR (255) NULL,
+author_artist VARCHAR (255) NULL,
 department_name VARCHAR (255) NULL,
 price DECIMAL (10,2) NULL,
 stock_quantity INTEGER NULL
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Eloquent Javascript", "Books", 16.95, 10), ("HTML & CSS", "Books", 22.95, 15), ("Javascript & JQuery", "Books", 22.95, 15), ("Theft By Finding", "Books", 35.99, 25), ("Helen & Troy's Epic Road Quest", "Books", 12.55, 100),
-("Starry Night", "Art", 150.00, 5), ("Vitruvian Man", "Art", 200.00, 10), ("Mona Lisa", "Art", 150.00, 6), ("David", "Art", 250.00, 10), ("Wheatfield with Crows", "Art", 250.00, 10),
-("I Wish I Had An Angel", "Music", 25.99, 150), ("Enter Sandman", "Music", 25.99, 125), ("Rapunzel", "Music", 18.99, 100), ("Snuff", "Music", 22.95, 140), ("Beethoven's Fifth Symphony", "Music", 35.99, 125);
+INSERT INTO products (product_name, author_artist, department_name, price, stock_quantity)
+VALUES ("Eloquent Javascript", "Haverbeke", "Books", 16.95, 140), ("HTML & CSS", "Duckett", "Books", 22.95, 150), ("Javascript & JQuery", "Duckett", "Books", 22.95, 135), ("Theft By Finding", "Sedaris", "Books", 35.99, 125), ("Helen & Troy's Epic Road Quest", "Martinez", "Books", 12.55, 100),
+("Starry Night (Print)", "Van Gogh", "Art", 149.99, 5), ("Wheatfield with Crows (Print)", "Van Gogh", "Art", 249.99, 10), ("Vitruvian Man (Print)", "Da Vinci", "Art", 199.99, 10), ("Mona Lisa (Print)", "Da Vinci", "Art", 149.99, 6), ("David (Statue)", "Michelangelo", "Art", 249.99, 10),
+("Once", "Nightwish", "Music", 25.99, 150), ("Metallica", "Metallica", "Music", 25.99, 125), ("Enchant", "Emilie Autumn", "Music", 18.99, 100), ("All Hope Is Gone", "Slipknot", "Music", 22.95, 140), ("Fifth Symphony", "Beethoven", "Music", 35.99, 125);
 
 SELECT * FROM products;
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,42 +1,170 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cli = require("pixl-cli");
+const chalk = require("chalk");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306.
-    port: 3306,
+  // Your port; if not 3306.
+  port: 3306,
 
-    // Your username.
-    user: "root",
+  // Your username.
+  user: "root",
 
-    // Your password.
-    password: "Y7A6G99L",
-    database: "bamazon_db"
+  // Your password.
+  password: "Y7A6G99L",
+  database: "bamazon_db"
 });
-
 
 // Connection ID.
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    itemsAvailable();
+  if (err) throw err;
 });
 
+function start() {
+  inquirer
+  .prompt({
+    name: "viewPurchase",
+    type: "list",
+    message: "Would you like to view available items or make a purchase?",
+    choices: ["View", "Purchase", "Exit"]
+  })
+  .then(function(answer) {
+    if (answer.viewPurchase === "View") {
+      itemsAvailable();
+      // purchaseOrExit();
+    }
+    else if(answer.viewPurchase === "Purchase") {
+      purchaseItem();
+    } else {
+      connection.end();
+    }
+  });
+}
+
+start();
+
+function purchaseOrExit() {
+  inquirer
+  .prompt({
+    name: "purchaseExit",
+    type: "list",
+    message: "Would you like to make a purchase or exit?",
+    choices: ["Purchase", "Exit"]
+  })
+  .then(function (response) {
+    if (response.purchaseExit === "Purchase") {
+      purchaseItem();
+    } else if (response.purchaseExit === "Exit") {
+      connection.end();
+    }
+  })
+}
 
 // Show customer all available products.
-function itemsAvailable() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " available");
-      }
-      console.log("-----------------------------------");
-    });
-    connection.end();
-  }
+var productInfo = [];
 
-// Ask customer ID number of item customer wants to buy.
+function itemsAvailable() { 
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // console.log("ID | Item | Department | Price | Units Available")
+    for (var i = 0; i < res.length; i++) {
+      productInfo.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].author_artist,
+        res[i].department_name,
+        "$" + res[i].price,
+        res[i].stock_quantity + " available",
+      ]);
+    }
+    var rows = [
+      ["ID", "Product", "Author/Artist", "Department", "Price", "Quantity"],
+      ...productInfo
+    ];
+    // Prints the inventory in a table format.
+    cli.print(
+      cli.table(rows) + "\n"
+    );
+    productInfo = []
+  });
+  purchaseOrExit();
+}
+
+function purchaseItem() {
+connection.query("SELECT * FROM products", function(err, res) {
+  if (err) throw err;
+  // once you have the items, prompt the user for which they'd like to bid on
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "rawlist",
+        choices: function() {
+          var choiceArray = [];
+          for (var i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].product_name);
+          }
+          return choiceArray;
+        },
+        message: "What item would you like to purchase? (Please use item ID.)"
+      },
+    ])
+    .then(function(answer) {
+      // get the information of the chosen item
+      var chosenItem;
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].item_id === answer.choice) {
+          chosenItem = res[i];
+        }
+      }
+
+      // // determine if bid was high enough
+      // if (chosenItem.highest_bid < parseInt(answer.bid)) {
+      //   // bid was high enough, so update db, let the user know, and start over
+      //   connection.query(
+      //     "UPDATE auctions SET ? WHERE ?",
+      //     [
+      //       {
+      //         highest_bid: answer.bid
+      //       },
+      //       {
+      //         id: chosenItem.id
+      //       }
+      //     ],
+      //     function(error) {
+      //       if (error) throw err;
+      //       console.log("Bid placed successfully!");
+      //       start();
+      //     }
+      //   );
+      // }
+      // else {
+      //   // bid wasn't high enough, so apologize and start over
+      //   console.log("Your bid was too low. Try again...");
+      }
+    );
+    })}
+
+
+
+// Ask customer if they would like to purchase anything, and ask for ID number of item.
+// function purchaseItem() {
+//   inquirer.prompt([{
+//       type: "input",
+//       name: "itemID",
+//       message: "What is the ID of the item you would like to purchase?"
+//     }, 
+//   ])
+//     .then(function (response) {
+//       if (response.itemPurchase) {
+//         response.itemID;
+//       } else {
+//         console.log("Press CTRL+C or CMD+C to exit.")
+//       }
+//     });
+// };
 
 // Ask customer how many units of item customer wants to buy.
 
